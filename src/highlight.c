6dd5a4ef52b34f414e37cfcb6e26b0b619045c5f
Meecoy <mikolaj.tryba@gmail.com>
2026-06-01 23:19:48 +0200

    Hotfix 2

--- src/highlight.c
+++ src/highlight.c
@@ -11,13 +11,13 @@ int is_separator(int c) {
 void update_syntax(editor_row *row) {
   row->hl = realloc(row->hl, row->render_size);
   memset(row->hl, HL_NORMAL, row->render_size);
-
+ 
   if (config.syntax == NULL) return;
   
   int prev_separator = 1;
   
   int i = 0;
-  while (i < row->size) {
+  while (i < row->render_size) {
     char c = row->render[i];
     unsigned char prev_hl = (i > 0) ? row->hl[i - 1] : HL_NORMAL;
     if (config.syntax->flags & HL_HIGHLIGHT_NUMBERS){
@@ -53,7 +53,8 @@ void select_highlight(){
     unsigned int j = 0;
     while (s->filematch[j]){
       int is_ext = (s->filematch[j][0] == '.');
-      if((is_ext && ext && !strcmp(ext, s->filematch[j])) || (!is_ext && strstr(config.filename, s->filematch[j]))){
+      if((is_ext && ext && !strcmp(ext, s->filematch[j])) ||
+	 (!is_ext && strstr(config.filename, s->filematch[j]))){
 	config.syntax = s;
 
 	for(int filerow = 0; filerow < config.numrows; filerow++) {
